edb46ba189284e178ece707486f169c23df0a353
Facu Conci <facu@example.com>
2020-11-20 19:35:42 -0300

    db connection & creation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProjectModule } from './project/project.module';
 import { ProjectService } from './project/project.service';
-import { ProjectModule } from './project/project.module';
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [ProjectModule],
+  imports: [ProjectModule,
+    MongooseModule.forRoot('mongodb://localhost/kanban-db',
+    { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true }),
+  ],
   controllers: [AppController],
   providers: [AppService, ProjectService],
 })
