b2e9a648220d35d6e4d3e8c8e89c82440d4c49fc
Facu Conci <facu@example.com>
2020-11-20 18:25:18 -0300

    project structure: projects resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProjectModule } from './project/project.module';
+import { ProjectService } from './project/project.service';
+import { ProjectModule } from './project/project.module';
 
 @Module({
-  imports: [],
+  imports: [ProjectModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ProjectService],
 })
 export class AppModule {}
