baaab7405ce640cf55ecbbd38a46deedfcd1e06a
Facu Conci <facu@example.com>
2020-11-25 15:21:56 -0300

    users: basic structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { ProjectModule } from './project/project.module';
 import { MongooseModule } from "@nestjs/mongoose";
 import { ConfigModule } from "@nestjs/config";
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [ProjectModule,
     ConfigModule.forRoot({ isGlobal: true, }),
     MongooseModule.forRoot(`mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`,
     { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
